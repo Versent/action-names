d64637599ad1443b6279959dbc9d28e9b764bc2e
Joshua Thornton <jshthornton@gmail.com>
2016-04-09 17:33:56 +0800

    Added support for having no aliases

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var snakeCase = require('lodash.snakecase');
 var trim      = require('lodash.trim');
 
-function addGroup(resource, actionTypes, group) {
+function addGroup(resource, actionTypes, group, addAlias) {
   var upperResource = snakeCase(resource).toUpperCase();
   var upperGroup    = group.toUpperCase();
 
@@ -15,22 +15,26 @@ function addGroup(resource, actionTypes, group) {
   actionTypes[start]   = start;
   actionTypes[success] = success;
   actionTypes[error]   = error;
-  actionTypes[startAlias] = start;
-  actionTypes[successAlias] = success;
-  actionTypes[errorAlias] = error;
+  
+  if (addAlias === true) {
+    actionTypes[startAlias] = start;
+    actionTypes[successAlias] = success;
+    actionTypes[errorAlias] = error;
+  }
 }
 
-module.exports = function(resource) {
+module.exports = function(resource, addAlias) {
   if (resource == null) throw new Error('Expected resource');
+  if (addAlias == null) addAlias = true;
 
   resource = trim(resource);
   if (resource == '')   throw new Error('Expected resource');
   var actionTypes = {};
 
-  addGroup(resource, actionTypes, 'fetch');
-  addGroup(resource, actionTypes, 'create');
-  addGroup(resource, actionTypes, 'update');
-  addGroup(resource, actionTypes, 'delete');
+  addGroup(resource, actionTypes, 'fetch', addAlias);
+  addGroup(resource, actionTypes, 'create', addAlias);
+  addGroup(resource, actionTypes, 'update', addAlias);
+  addGroup(resource, actionTypes, 'delete', addAlias);
 
   return actionTypes;
 }
